a57680579285834d9354cf3c697eff08ee3a5169
Meredith Culberson <maculberson09@icloud.com>
2024-11-21 16:33:43 -0600

    testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/autoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/autoTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.RobotHardware;
 
+// lemoine was here
 
 @Autonomous(name = "autoTest", group = "Robot")
 
